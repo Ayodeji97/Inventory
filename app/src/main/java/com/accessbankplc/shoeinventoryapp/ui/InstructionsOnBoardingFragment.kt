8ca26e03de34e54d0621721603ebc70b87c4e750
admin <admin@admins-MacBook-Pro.local>
2021-05-04 23:49:21 +0100

    added style to project

---
+++ app/src/main/java/com/accessbankplc/shoeinventoryapp/ui/InstructionsOnBoardingFragment.kt
@@ -0,0 +1,20 @@
+package com.accessbankplc.shoeinventoryapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.accessbankplc.shoeinventoryapp.R
+
+
+class InstructionsOnBoardingFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_instructions_on_boarding, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/accessbankplc/shoeinventoryapp/ui/WelcomeOnBoardingFragment.kt
@@ -0,0 +1,20 @@
+package com.accessbankplc.shoeinventoryapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.accessbankplc.shoeinventoryapp.R
+
+class WelcomeOnBoardingFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome_on_boarding, container, false)
+    }
+}
